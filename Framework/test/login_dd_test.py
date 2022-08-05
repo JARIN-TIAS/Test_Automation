ae735220fa7c61cd52d4603a97fa5f786241813e
JARIN-TIAS <gmeraj95@gmail.com>
2022-08-05 19:08:55 +0600

    Merge remote-tracking branch 'origin/master'

--- Framework/test/login_dd_test.py
+++ Framework/test/login_dd_test.py
@@ -21,6 +21,7 @@ class LoginTest(unittest.TestCase):
         sheet = 'Sheet1'
 
         number_of_rows = excel_utils.get_row_count(file, sheet)
+        print(number_of_rows)
 
         for r in range(2, number_of_rows + 1):
             username = excel_utils.read_data(file, sheet, r, 1)
